84ad9dbfeadac533ebb949c4f3710855b3f60e2a
Fellyipe <felipebueno2004@hotmail.com>
2024-03-15 15:22:14 -0300

    Cópia TPTE01

---
+++ TPTE02/Projeto02/Models/ProdutoDto.cs
@@ -0,0 +1,10 @@
+namespace Projeto02.Models
+{
+    public class ProdutoDto
+    {
+        public string Nome { get; set; } = "";
+        public string Descricao { get; set; } = "";
+        public double Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+}
